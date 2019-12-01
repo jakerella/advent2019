1f4abe6494cccc421f4cffb815646320cd4b3a5b
jakerella <jordan@jordankasper.com>
2019-12-01 08:59:29 -0500

    Finished up part 2 of day 1

--- day01/day-01.go
+++ day01/day-01.go
@@ -9,20 +9,6 @@ import (
 	"math"
 )
 
-/*
-Fuel required to launch a given module is based on its mass. Specifically, to find the fuel required for a module, take its mass, divide by three, round down, and subtract 2.
-
-For example:
-
-For a mass of 12, divide by 3 and round down to get 4, then subtract 2 to get 2.
-For a mass of 14, dividing by 3 and rounding down still yields 4, so the fuel required is also 2.
-For a mass of 1969, the fuel required is 654.
-For a mass of 100756, the fuel required is 33583.
-The Fuel Counter-Upper needs to know the total fuel requirement. To find it, individually calculate the fuel needed for the mass of each module (your puzzle input), then add together all the fuel values.
-
-What is the sum of the fuel requirements for all of the modules on your spacecraft?
-*/
-
 func main() {
 	helpers.WriteHeader(1)
 	
@@ -46,12 +32,21 @@ func main() {
 	totalFuel := 0
 	for _, mod := range modules {
 		// For a mass of 12, divide by 3 and round down to get 4, then subtract 2 to get 2.
-		var div float64 = float64(mod / 3)
-		fuel := int(math.Floor(div)) - 2
-		totalFuel += fuel
-		helpers.Log(strconv.Itoa(mod) + " uses " + strconv.Itoa(fuel))
+		moduleFuel := fuelForWeight(mod)
+		fuelForFuel := fuelForWeight(moduleFuel)
+		for fuelForFuel > 0 {
+			moduleFuel += fuelForFuel
+			fuelForFuel = fuelForWeight(fuelForFuel)
+		}
+		totalFuel += moduleFuel
+		helpers.Log(strconv.Itoa(mod) + " uses " + strconv.Itoa(moduleFuel))
 	}
 
-	helpers.Log("Answer: " + strconv.Itoa(totalFuel))
+	helpers.Log("Fuel Required: " + strconv.Itoa(totalFuel))
+}
 
+func fuelForWeight(amt int) int {
+	var div float64 = float64(amt / 3)
+	fuel := int(math.Floor(div)) - 2
+	return fuel
 }
