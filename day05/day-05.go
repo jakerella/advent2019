69da4279c5fe3c372f3a26782a8efa995a23a673
jakerella <jordan@jordankasper.com>
2019-12-06 06:46:19 -0500

    Added day 5, part 2... finally

--- day05/day-05.go
+++ day05/day-05.go
@@ -13,7 +13,7 @@ import (
 func main() {
 	h.WriteHeader(2)
 	
-	if len(os.Args) < 2 { h.OhShit(errors.New("Please provide the input filename")) }
+	if len(os.Args) < 3 { h.OhShit(errors.New("Please provide the filename and input value")) }
 
 	input, err := os.Open(os.Args[1])
 	if err != nil { h.OhShit(err) }
@@ -24,7 +24,8 @@ func main() {
 
 	partOne := append([]int(nil), program...)
 
-	var programInput int = 1
+	programInput, err := strconv.Atoi(os.Args[2])
+	if err != nil { h.OhShit(err) }
 
 	value, err := intcode(partOne, programInput)
 	if err != nil { h.OhShit(err) }
@@ -41,6 +42,8 @@ func intcode(program []int, input int) (value int, err error) {
 	
 	var address int = 0
 	for program[address] != 99 {
+		// log.Info(program)
+
 		code := strconv.Itoa(program[address])
 		var instruction int
 		var paramTypes = [3]int{0,0,0}
@@ -85,6 +88,41 @@ func intcode(program []int, input int) (value int, err error) {
 			if paramTypes[0] == 0 { param = program[program[address+1]] } else { param = program[address+1] }
 			log.Info("Output instruction: ", param)
 			address += 2
+		
+		case 5:
+			test, value := 0, 0
+			if paramTypes[0] == 0 { test = program[program[address+1]] } else { test = program[address+1] }
+			if paramTypes[1] == 0 { value = program[program[address+2]] } else { value = program[address+2] }
+			if test != 0 {
+				address = value
+			} else {
+				address += 3
+			}
+
+		case 6:
+			test, value := 0, 0
+			if paramTypes[0] == 0 { test = program[program[address+1]] } else { test = program[address+1] }
+			if paramTypes[1] == 0 { value = program[program[address+2]] } else { value = program[address+2] }
+			if test == 0 {
+				address = value
+			} else {
+				address += 3
+			}
+
+		case 7:
+			x, y := 0, 0
+			if paramTypes[0] == 0 { x = program[program[address+1]] } else { x = program[address+1] }
+			if paramTypes[1] == 0 { y = program[program[address+2]] } else { y = program[address+2] }
+			if x < y { program[program[address+3]] = 1 } else { program[program[address+3]] = 0 }
+			address += 4
+		
+		case 8:
+			x, y := 0, 0
+			if paramTypes[0] == 0 { x = program[program[address+1]] } else { x = program[address+1] }
+			if paramTypes[1] == 0 { y = program[program[address+2]] } else { y = program[address+2] }
+			if x == y { program[program[address+3]] = 1 } else { program[program[address+3]] = 0 }
+			address += 4
+		
 		default:
 			return 0, errors.New("Unknown instruction: " + strconv.Itoa(instruction) + " processing code: " + strconv.Itoa(program[address]) + " - " + code)
 		}
