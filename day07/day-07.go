23071144148ad3731ac4deed670ba806c94528da
jakerella <jordan@jordankasper.com>
2019-12-07 13:16:29 -0500

    Day 7, part 2!

--- day07/day-07.go
+++ day07/day-07.go
@@ -12,6 +12,15 @@ import (
 
 var phases = [][]int{}
 
+type Program struct {
+	state []int
+	address int
+	inputs [2]int
+	inputIndex int
+	lastOutput int
+	complete bool
+}
+
 func main() {
 	h.WriteHeader(7)
 	
@@ -25,28 +34,55 @@ func main() {
 	h.OhShit(json.NewDecoder(input).Decode(&program))
 
 	// generate all sequences of phases
-	possiblePhases := []int{0,1,2,3,4}
+	possiblePhases := []int{5,6,7,8,9}
 	phasePermutations(possiblePhases, len(possiblePhases), len(possiblePhases))
+	// phases = append(phases, []int{9,8,7,6,5})
 	// log.Info(phases)
-	
+
 	holdOuput := 0
 	phaseSeq := []int{}
 	for _, phase := range phases {
-		inputSignal := 0
+		programs := []Program{}
+		currentProgram := 0
+
+		// Create each program with correct phase input
 		for _, phaseInput := range phase {
-			outputs, err := intcode(append([]int(nil), program...), []int{phaseInput, inputSignal})
+			programs = append(programs, Program{ append([]int(nil), program...), 0, [2]int{phaseInput, 0}, 0, -999, false })
+		}
+
+		// i := 0  // infinity check
+		for {  // i<40 {
+			// i++
+
+			// log.Info("RUN ", currentProgram, ": ", programs[currentProgram].inputs, programs[currentProgram].address, programs[currentProgram].complete)
+			programState, err := intcode(programs[currentProgram])
+			programs[currentProgram] = programState
 			if err != nil { h.OhShit(err) }
 			
-			// log.Info("Outputs", outputs)
-			if outputs[0] > holdOuput {
-				holdOuput = outputs[0]
-				phaseSeq = phase
+			// log.Info("END ", currentProgram, ": ", programs[currentProgram].lastOutput, programs[currentProgram].address, programs[currentProgram].complete)
+			if programs[currentProgram].complete {
+				// log.Info("PROGRAM ", currentProgram, " END: ", programs[currentProgram].lastOutput, programs[currentProgram].complete)
+				if programs[currentProgram].lastOutput > holdOuput {
+					holdOuput = programs[currentProgram].lastOutput
+					phaseSeq = phase
+				}
+				if currentProgram == len(programs) - 1 { break }
 			}
 
-			inputSignal = outputs[0]
+			currentProgram++
+			if currentProgram >= len(programs) { currentProgram = 0 }
+			programs[currentProgram].inputs[1] = programState.lastOutput
+
+			// kick off program A with its phase input and signal (0)
+			// when program A hits an output, save program and address
+			// use output of A as input signal to kick off B
+			// repeat for C, D, E
+			// keep track of current program index
+			// as programs output, send input to next program index and resume until output
+			// as programs terminate, stop sending input
 		}
 	}
-	log.Info("Largest Output (p1): ", holdOuput, " from phase seq ", phaseSeq)
+	log.Info("Largest Output (p2): ", holdOuput, " from phase seq ", phaseSeq)
 }
 
 
@@ -70,26 +106,23 @@ func phasePermutations(seq []int, size int, count int) {
 }
 
 
-func intcode(program []int, input []int) (value []int, err error) {
+func intcode(program Program) (p Program, err error) {
 	defer func() {
         if (recover() != nil) {
             err = errors.New("Address out of bounds")
         }
 	}()
-	
-	outputs := []int{}
 
-	// log.Info("Inputs: ", input)
+	// log.Info("Input: ", input)
 
-	var address int = 0
-	for program[address] != 99 {
-		// log.Info(program)
+	for program.state[program.address] != 99 {
+		// log.Trace(program)
 
-		code := strconv.Itoa(program[address])
+		code := strconv.Itoa(program.state[program.address])
 		var instruction int
 		var paramTypes = [3]int{0,0,0}
-		if program[address] < 100 {
-			instruction = program[address]
+		if program.state[program.address] < 100 {
+			instruction = program.state[program.address]
 		} else {
 			if len(code) == 3 { code = "00" + code }
 			if len(code) == 4 { code = "0" + code }
@@ -105,71 +138,74 @@ func intcode(program []int, input []int) (value []int, err error) {
 			if err != nil { h.OhShit(err) }
 		}
 
+		// log.Info("Instruction: ", instruction, paramTypes)
+
 		switch instruction {
 		case 1:
 			x, y := 0, 0
-			if paramTypes[0] == 0 { x = program[program[address+1]] } else { x = program[address+1] }
-			if paramTypes[1] == 0 { y = program[program[address+2]] } else { y = program[address+2] }
-			program[program[address+3]] = x + y
-			address += 4
+			if paramTypes[0] == 0 { x = program.state[program.state[program.address+1]] } else { x = program.state[program.address+1] }
+			if paramTypes[1] == 0 { y = program.state[program.state[program.address+2]] } else { y = program.state[program.address+2] }
+			program.state[program.state[program.address+3]] = x + y
+			program.address += 4
 
 		case 2:
 			x, y := 0, 0
-			if paramTypes[0] == 0 { x = program[program[address+1]] } else { x = program[address+1] }
-			if paramTypes[1] == 0 { y = program[program[address+2]] } else { y = program[address+2] }
-			program[program[address+3]] = x * y
-			address += 4
+			if paramTypes[0] == 0 { x = program.state[program.state[program.address+1]] } else { x = program.state[program.address+1] }
+			if paramTypes[1] == 0 { y = program.state[program.state[program.address+2]] } else { y = program.state[program.address+2] }
+			program.state[program.state[program.address+3]] = x * y
+			program.address += 4
 
 		case 3:
-			next := input[0]
-			input = input[1:]
-			program[program[address+1]] = next
-			address += 2
+			program.state[program.state[program.address+1]] = program.inputs[program.inputIndex]
+			if program.inputIndex == 0 { program.inputIndex++ }
+			program.address += 2
 
 		case 4:
-			param := 0
-			if paramTypes[0] == 0 { param = program[program[address+1]] } else { param = program[address+1] }
-			// log.Info("Output instruction: ", param)
-			outputs = append(outputs, param)
-			address += 2
+			output := -9999
+			if paramTypes[0] == 0 { output = program.state[program.state[program.address+1]] } else { output = program.state[program.address+1] }
+			program.lastOutput = output
+			// log.Info("Output instruction: ", program.lastOutput)
+			program.address += 2
+			return program, nil
 		
 		case 5:
 			test, value := 0, 0
-			if paramTypes[0] == 0 { test = program[program[address+1]] } else { test = program[address+1] }
-			if paramTypes[1] == 0 { value = program[program[address+2]] } else { value = program[address+2] }
+			if paramTypes[0] == 0 { test = program.state[program.state[program.address+1]] } else { test = program.state[program.address+1] }
+			if paramTypes[1] == 0 { value = program.state[program.state[program.address+2]] } else { value = program.state[program.address+2] }
 			if test != 0 {
-				address = value
+				program.address = value
 			} else {
-				address += 3
+				program.address += 3
 			}
 
 		case 6:
 			test, value := 0, 0
-			if paramTypes[0] == 0 { test = program[program[address+1]] } else { test = program[address+1] }
-			if paramTypes[1] == 0 { value = program[program[address+2]] } else { value = program[address+2] }
+			if paramTypes[0] == 0 { test = program.state[program.state[program.address+1]] } else { test = program.state[program.address+1] }
+			if paramTypes[1] == 0 { value = program.state[program.state[program.address+2]] } else { value = program.state[program.address+2] }
 			if test == 0 {
-				address = value
+				program.address = value
 			} else {
-				address += 3
+				program.address += 3
 			}
 
 		case 7:
 			x, y := 0, 0
-			if paramTypes[0] == 0 { x = program[program[address+1]] } else { x = program[address+1] }
-			if paramTypes[1] == 0 { y = program[program[address+2]] } else { y = program[address+2] }
-			if x < y { program[program[address+3]] = 1 } else { program[program[address+3]] = 0 }
-			address += 4
+			if paramTypes[0] == 0 { x = program.state[program.state[program.address+1]] } else { x = program.state[program.address+1] }
+			if paramTypes[1] == 0 { y = program.state[program.state[program.address+2]] } else { y = program.state[program.address+2] }
+			if x < y { program.state[program.state[program.address+3]] = 1 } else { program.state[program.state[program.address+3]] = 0 }
+			program.address += 4
 		
 		case 8:
 			x, y := 0, 0
-			if paramTypes[0] == 0 { x = program[program[address+1]] } else { x = program[address+1] }
-			if paramTypes[1] == 0 { y = program[program[address+2]] } else { y = program[address+2] }
-			if x == y { program[program[address+3]] = 1 } else { program[program[address+3]] = 0 }
-			address += 4
+			if paramTypes[0] == 0 { x = program.state[program.state[program.address+1]] } else { x = program.state[program.address+1] }
+			if paramTypes[1] == 0 { y = program.state[program.state[program.address+2]] } else { y = program.state[program.address+2] }
+			if x == y { program.state[program.state[program.address+3]] = 1 } else { program.state[program.state[program.address+3]] = 0 }
+			program.address += 4
 		
 		default:
-			return nil, errors.New("Unknown instruction: " + strconv.Itoa(instruction) + " processing code: " + strconv.Itoa(program[address]) + " - " + code)
+			return program, errors.New("Unknown instruction: " + strconv.Itoa(instruction) + " processing code: " + strconv.Itoa(program.state[program.address]) + " - " + code)
 		}
 	}
-	return outputs, nil
+	program.complete = true
+	return program, nil
 }
